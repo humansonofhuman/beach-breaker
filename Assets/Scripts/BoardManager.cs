400e18e316c032ba9f71084cf7a2b894005b4d82
Mateo Morales García <mateomogar@gmail.com>
2020-10-26 13:21:01 -0500

    Add BoardManager

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager sharedInstance;
+    public List<Sprite> prefabs = new List<Sprite>();
+    public GameObject currentCandy;
+    public int xSize, ySize;
+
+    private GameObject[,] candies;
+
+    public bool isShifting { get; set; }
+
+    void Start()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
