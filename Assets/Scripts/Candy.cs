ecf3d4e46502afcd3bd575c25a2744edd0be77f0
Mateo Morales García <mateomogar@gmail.com>
2020-10-26 13:03:22 -0500

    Prepare candy script

--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Candy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static Color selectedColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+    private static Candy previousSelected = null;
+
+    public int id;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isSelected = false;
+    private Vector2[] adjacentDirections = new Vector2[]
     {
-        
+        Vector2.up,
+        Vector2.down,
+        Vector2.left,
+        Vector2.right,
+    };
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
     void Update()
     {
-        
+
     }
 }
