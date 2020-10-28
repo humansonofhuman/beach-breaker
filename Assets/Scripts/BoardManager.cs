66d7ba4028a230a3d780361ee1c80e4bc1105892
Mateo Morales García <mateomogar@gmail.com>
2020-10-28 17:04:57 -0500

    Add CreateInitialBoard

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -23,6 +23,32 @@ public class BoardManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        Vector2 offset = currentCandy.GetComponent<BoxCollider2D>().size;
+        CreateInitialBoard(offset);
+    }
+
+    private void CreateInitialBoard(Vector2 offset)
+    {
+        candies = new GameObject[xSize, ySize];
+
+        float startX = this.transform.position.x;
+        float startY = this.transform.position.y;
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                var position = new Vector3(startX + (offset.x * x),
+                                           startY + (offset.y * y),
+                                           0);
+                GameObject newCandy = Instantiate(currentCandy,
+                                                  position,
+                                                  currentCandy.transform.rotation);
+                newCandy.name = $"Candy [{x}] [{y}]";
+                candies[x, y] = newCandy;
+            }
+        }
     }
 
     void Update()
