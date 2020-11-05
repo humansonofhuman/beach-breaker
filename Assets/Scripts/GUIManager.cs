72983320d5f201f9504167b223bb9347e7d88509
Mateo Morales García <mateomogar@gmail.com>
2020-11-05 00:08:59 -0500

    Configure GUIManager

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GUIManager sharedInstance;
+    public Text movesText;
+    public Text scoreText;
+    private int moveCounter;
+    private int score;
+    public int MoveCounter
+    {
+        get
+        {
+            return moveCounter;
+        }
+        set
+        {
+            moveCounter = value;
+            movesText.text = $"Moves: {moveCounter}";
+        }
+    }
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+            scoreText.text = $"Score: {score}";
+        }
+    }
     void Start()
     {
-        
+        if (sharedInstance == null) { sharedInstance = this; }
+        else { Destroy(gameObject); }
+
+        Score = 0;
+        MoveCounter = 30;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
