6a2b93c5a6bc3b9d76c1f69a66f2c86776f3a876
Mateo Morales García <mateomogar@gmail.com>
2020-11-16 14:53:25 -0500

    Add new canvas organization
    Add CanvasManager to manage and switch between the subcanvas
    
    Add CanvasController to identify the sub canvas inside the main canvas
    
    Add 3 CanvasType

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum CanvasType
+{
+    MainMenu,
+    GameUI,
+    GameOver,
+}
+
+public class CanvasManager : Singleton<CanvasManager>
+{
+    List<CanvasController> canvasControllers;
+    CanvasController lastActiveCanvas;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        canvasControllers = GetComponentsInChildren<CanvasController>()
+                            .ToList();
+        canvasControllers.ForEach(c => c.gameObject.SetActive(false));
+        SwitchCanvas(CanvasType.MainMenu);
+    }
+
+    public void SwitchCanvas(CanvasType type)
+    {
+        CanvasController desiredCanvas = canvasControllers.Find(c => c.canvasType == type);
+        if (desiredCanvas != null)
+        {
+            if (lastActiveCanvas != null)
+            {
+                lastActiveCanvas.gameObject.SetActive(false);
+            }
+
+            desiredCanvas.gameObject.SetActive(true);
+            lastActiveCanvas = desiredCanvas;
+        }
+        else { Debug.LogWarning("The desired canvas was not found!"); }
+    }
+}
