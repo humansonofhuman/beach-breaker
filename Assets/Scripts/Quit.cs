9ded8967aee251fc22da4352974371b079d17840
Mateo Morales García <mateomogar@gmail.com>
2020-11-16 15:06:21 -0500

    Add quit script for button

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Quit : MonoBehaviour
+{
+    Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClicked);
+    }
+
+    void OnButtonClicked()
+    {
+        ExitGame();
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
