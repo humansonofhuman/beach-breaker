de44e4f6eb792459a0af9e48e9fa3022db1750be
Mateo Morales García <mateomogar@gmail.com>
2020-11-16 14:55:39 -0500

    Add canvasSwitcher
    script with behavior for a button that changes from one canvas to other
    
    learned from: https://youtu.be/U8XiDji23U0

---
+++ Assets/Scripts/CanvasSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class CanvasSwitcher : MonoBehaviour
+{
+    public CanvasType desiredCanvasType;
+
+    CanvasManager canvasManager;
+    Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnButtonClicked);
+        canvasManager = CanvasManager.GetInstance();
+    }
+
+    void OnButtonClicked()
+    {
+        canvasManager.SwitchCanvas(desiredCanvasType);
+    }
+
+}
